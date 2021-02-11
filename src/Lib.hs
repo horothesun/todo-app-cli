05e52011bc6ff414475bb562e0c5fc25ec6a75d2
Alessandro Candolini <alessandrocandolini@gmail.com>
2021-02-11 20:57:14 +0000

    feb 11

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,28 +3,62 @@ module Lib where
 import Control.Monad (mfilter)
 import Data.Char (isSpace)
 import Data.List (dropWhileEnd)
+import Data.String
 import Data.Text (strip)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data TodoResult s a
+  = TitleEmpty
+  | PastDueDate
+  | Todo
+      { description :: s,
+        dueDate :: Maybe a,
+        creationDate :: a
+      }
+  deriving (Eq, Show)
+
+-- checkTodo :: (Eq a, IsString a) => a -> p -> TodoResult a p
+checkTodo :: String -> p -> TodoResult String p
+checkTodo s _ | (null . trim) s = TitleEmpty
+checkTodo _ _ = PastDueDate
+
+checkTodo2 :: s -> a -> TodoResult s a
+checkTodo2 = undefined
+
+-- " 1 " -> "1"
+trim :: String -> String
+trim = trimRight . trimLeft
+
+-- trimRight = reverse . trimLeft . reverse
+
+trimLeft :: String -> String
+-- trimLeft = dropWhile isSpace
+trimLeft s = fst (myfold accumulate s ("", True))
+  where
+    accumulate c (s, b)
+      | isSpace c && b = (s, b)
+      | otherwise = ([c] ++ s, False)
+
+size [] = 0
+size (_ : t) = 1 + size t
+
+myfold f [] a = a
+myfold f (h : t) a = f h (myfold f t a)
+
+trimRight = dropWhileEnd isSpace
+
+{-}
+trimLeft [] = []
+trimLeft s@(h : t)
+  | isSpace h = trimLeft t
+  | otherwise = s
+-}
+-- OLD
 
 data Title = InvalidTitle | ValidTitle String deriving (Eq, Show)
 
 isTitleValid :: String -> Title
 isTitleValid "" = InvalidTitle
 isTitleValid t = ValidTitle t
-
-nonEmptyTitle "" = Nothing
-nonEmptyTitle s = Just s
-
-title = title2
-
-title2 = nonEmptyTitle . trim
-
-title3 = nonEmptyTitle . strip
-
-trim = dropWhileEnd isSpace . dropWhile isSpace
-
--- Approach 3
-
-f' s = mfilter (all isSpace) (Just s)
--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -2,24 +2,50 @@ module LibSpec where
 
 import Data.Char
 import Data.Maybe (isJust, isNothing)
-import Lib (title)
+import Lib
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
 import Test.QuickCheck.Property
 
-spaceCharGen :: Gen Char
-spaceCharGen = suchThat arbitrary isSpace
+notEmptyStringGen :: Gen String
+notEmptyStringGen = suchThat arbitrary (not . (any isSpace))
 
-emptyOrBlankStringGen :: Gen String
-emptyOrBlankStringGen = listOf spaceCharGen
+emptyOrBlankGen :: Gen String
+-- emptyOrBlankGen =  suchThat arbitrary (all isSpace)
+emptyOrBlankGen = listOf spaceCharGen
+  where
+    spaceCharGen :: Gen Char
+    spaceCharGen = suchThat arbitrary isSpace
 
 spec :: Spec
 spec = describe "TodoApp tests" $ do
-  modifyMaxSuccess (const 5000) $
-    prop "title is not valid if empty or blank" $
-      forAll emptyOrBlankStringGen $ \t -> isNothing $ title t
+  describe "checkTodo" $ do
+    -- prop "check that the new implementation is consistent with the legacy one" $
+    --  \s a -> (checkTodo (s :: String) (a :: Int)) == (checkTodo2 s a)
 
-  modifyMaxSuccess (const 5000) $
-    prop "title is valid whenver it's not empty or blank" $
-      \t -> (not . all isSpace) t ==> isJust $ title t
+    prop "must return invalid title IF title is empty or blank" $
+      forAll emptyOrBlankGen $ \s ->
+        forAll arbitrary $ \a ->
+          checkTodo (s :: String) (a :: Int) == TitleEmpty
+
+    prop "must return invalid title ONLY IF title is empty or blank" $
+      \s a -> (not . all isSpace) s ==> checkTodo s (a :: Int) /= TitleEmpty
+
+  describe "trim" $ do
+    prop "must be idempotent" $
+      \s -> (trim . trim) s == trim s
+
+    prop "must return empty string if the original string is empty or blank" $
+      forAll emptyOrBlankGen $ \s ->
+        trim s == ""
+
+    prop "must remove empty strings at the end and at the beginning" $
+      forAll emptyOrBlankGen $ \s1 ->
+        forAll emptyOrBlankGen $ \s2 ->
+          forAll notEmptyStringGen $ \s3 ->
+            (not . null) s1 && (not . null) s2 && (not . null) s3
+              ==> trim (s1 ++ s3 ++ s2) == s3
+
+    it "remove spaces at the beginning" $
+      (trim "  he ll  o ") `shouldBe` "he ll  o"
