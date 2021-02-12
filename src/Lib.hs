aab62571e5d8a8e4402ac9bdef0eb4bf0a79ecc6
Alessandro Candolini <alessandrocandolini@gmail.com>
2021-02-12 02:16:39 +0000

    cleanup

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,7 @@
 module Lib where
 
-import Control.Monad (mfilter)
 import Data.Char (isSpace)
 import Data.List (dropWhileEnd)
-import Data.String
-import Data.Text (strip)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -27,34 +24,13 @@ checkTodo _ _ = PastDueDate
 checkTodo2 :: s -> a -> TodoResult s a
 checkTodo2 = undefined
 
--- " 1 " -> "1"
 trim :: String -> String
 trim = trimRight . trimLeft
-
--- trimRight = reverse . trimLeft . reverse
-
-trimLeft :: String -> String
--- trimLeft = dropWhile isSpace
-trimLeft s = fst (myfold accumulate s ("", True))
   where
-    accumulate c (s, b)
-      | isSpace c && b = (s, b)
-      | otherwise = ([c] ++ s, False)
+    -- trimRight = reverse . trimLeft . reverse
+    trimRight = dropWhileEnd isSpace
+    trimLeft = dropWhile isSpace
 
-size [] = 0
-size (_ : t) = 1 + size t
-
-myfold f [] a = a
-myfold f (h : t) a = f h (myfold f t a)
-
-trimRight = dropWhileEnd isSpace
-
-{-}
-trimLeft [] = []
-trimLeft s@(h : t)
-  | isSpace h = trimLeft t
-  | otherwise = s
--}
 -- OLD
 
 data Title = InvalidTitle | ValidTitle String deriving (Eq, Show)
--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -1,18 +1,15 @@
 module LibSpec where
 
 import Data.Char
-import Data.Maybe (isJust, isNothing)
 import Lib
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
-import Test.QuickCheck.Property
 
 notEmptyStringGen :: Gen String
-notEmptyStringGen = suchThat arbitrary (not . (any isSpace))
+notEmptyStringGen = suchThat arbitrary (not . any isSpace)
 
 emptyOrBlankGen :: Gen String
--- emptyOrBlankGen =  suchThat arbitrary (all isSpace)
 emptyOrBlankGen = listOf spaceCharGen
   where
     spaceCharGen :: Gen Char
@@ -36,16 +33,20 @@ spec = describe "TodoApp tests" $ do
     prop "must be idempotent" $
       \s -> (trim . trim) s == trim s
 
-    prop "must return empty string if the original string is empty or blank" $
+    prop "must always return empty string if the original string is empty or blank" $
       forAll emptyOrBlankGen $ \s ->
         trim s == ""
 
-    prop "must remove empty strings at the end and at the beginning" $
-      forAll emptyOrBlankGen $ \s1 ->
-        forAll emptyOrBlankGen $ \s2 ->
-          forAll notEmptyStringGen $ \s3 ->
-            (not . null) s1 && (not . null) s2 && (not . null) s3
-              ==> trim (s1 ++ s3 ++ s2) == s3
+    prop "must behave like the identify on the subset of strings not containing space characters" $
+      forAll notEmptyStringGen $ \s -> trim s == s
 
-    it "remove spaces at the beginning" $
-      (trim "  he ll  o ") `shouldBe` "he ll  o"
+    prop "must remove empty strings at the end and at the beginning but not in the middle" $
+      forAll (suchThat emptyOrBlankGen (not . null)) $ \s1 ->
+        forAll (suchThat notEmptyStringGen (not . null)) $ \s2 ->
+          forAll (suchThat emptyOrBlankGen (not . null)) $ \s3 ->
+            forAll (suchThat notEmptyStringGen (not . null)) $ \s4 ->
+              forAll (suchThat emptyOrBlankGen (not . null)) $ \s5 ->
+                trim (s1 ++ s2 ++ s3 ++ s4 ++ s5) == s2 ++ s3 ++ s4
+
+    it "for example, must remove spaces at the beginning and at the end but not in the middle" $
+      trim "  he ll  o " `shouldBe` "he ll  o"
