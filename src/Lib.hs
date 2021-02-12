a9d7e51ea458348b421a5ff5e2df9e0f86aa50a3
Alessandro Candolini <alessandrocandolini@gmail.com>
2021-02-12 17:30:32 +0000

    Re-architecture the approach with PBTs and Eithers (#2)

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,66 @@
-module Lib where
+module Lib
+  ( someFunc,
+    TodoValidationError (..),
+    checkTodo,
+    checkTodo',
+    trim,
+    trimRight,
+    trimLeft,
+    Todo (..),
+  )
+where
+
+import Control.Monad
+import Data.Char (isSpace)
+import Data.Either.Combinators (mapLeft, maybeToRight)
+import Data.List (dropWhileEnd)
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  putStrLn "someFunc"
 
-data Title = InvalidTitle | ValidTitle String deriving (Eq, Show)
+data Todo s a = Todo
+  { description :: s,
+    dueDate :: Maybe a,
+    creationDate :: a
+  }
+  deriving (Eq, Show)
 
-isTitleValid :: String -> Title
-isTitleValid "" = InvalidTitle
--- isTitleValid " " = InvalidTitle
-isTitleValid t  = ValidTitle t
+data TodoValidationError = TitleEmpty | PastDueDate deriving (Eq, Show)
 
+checkTodo :: Ord p => String -> Maybe p -> p -> Either TodoValidationError (Todo String p)
+checkTodo = checkTodo1
+
+checkTodo' :: Ord p => String -> Maybe p -> p -> Either TodoValidationError (Todo String p)
+checkTodo' = checkTodo2
+
+-- checkTodo :: (Eq a, IsString a) => a -> p -> TodoResult a p
+checkTodo1 :: Ord p => String -> Maybe p -> p -> Either TodoValidationError (Todo String p)
+checkTodo1 s _ _ | (null . trim) s = Left TitleEmpty
+checkTodo1 _ (Just a) b | a < b = Left PastDueDate
+checkTodo1 s a b = Right $ Todo s a b
+
+checkTodo2 :: Ord p => String -> Maybe p -> p -> Either TodoValidationError (Todo String p)
+checkTodo2 s a b = do
+  t <- maybeToRight TitleEmpty (checkTitle s)
+  d <- mapLeft (const PastDueDate) (checkDueDate b a)
+  return $ Todo t d b
+
+checkTitle :: String -> Maybe String
+checkTitle = mfilter (not . null . trim) . Just
+
+checkDueDate :: Ord p => p -> Maybe p -> Either () (Maybe p)
+checkDueDate n = traverse f
+  where
+    f d
+      | d < n = Left ()
+      | otherwise = Right d
+
+trim :: String -> String
+trim = trimRight . trimLeft
+
+trimRight :: String -> String
+trimRight = dropWhileEnd isSpace
+
+trimLeft :: String -> String
+trimLeft = dropWhile isSpace
--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -1,28 +1,84 @@
 module LibSpec where
 
-import Test.Hspec
-import Test.Hspec.QuickCheck
-import Test.QuickCheck.Property
+import Data.Char (isSpace)
+import Data.Either (isRight)
 import Lib
+  ( TodoValidationError (PastDueDate, TitleEmpty),
+    checkTodo,
+    checkTodo',
+    trim,
+    trimLeft,
+    trimRight,
+  )
+import Test.Hspec (Spec, describe, it, shouldBe)
+import Test.Hspec.QuickCheck (modifyMaxSuccess, prop)
 import Test.QuickCheck
--- import Test.QuickCheck.Gen
+  ( Arbitrary (arbitrary),
+    Gen,
+    forAll,
+    listOf,
+    suchThat,
+    (==>),
+  )
 
--- simpleGen :: Gen String
--- simpleGen = ...
+notEmptyStringGen :: Gen String
+notEmptyStringGen = suchThat arbitrary (not . any isSpace)
 
-positiveNumberGen :: Gen Int
-positiveNumberGen = fmap abs arbitrary
---                  baseGen.map(i => return abs(i))
---                  baseGen.map(abs)
+emptyOrBlankGen :: Gen String
+emptyOrBlankGen = listOf spaceCharGen
+  where
+    spaceCharGen :: Gen Char
+    spaceCharGen = suchThat arbitrary isSpace
 
 spec :: Spec
 spec = describe "TodoApp tests" $ do
+  describe "checkTodo" $ do
+    modifyMaxSuccess (const 5000) $
+      prop "check that the new implementation is consistent with the legacy one (this test is just an example)" $
+        \s a b -> checkTodo (s :: String) a (b :: Int) == checkTodo' s a b
 
-     it "title is not valid if empty" $
-         isTitleValid "" `shouldBe` InvalidTitle
+    prop "must return invalid title IF title is empty or blank" $
+      forAll emptyOrBlankGen $ \s ->
+        forAll arbitrary $ \a ->
+          forAll arbitrary $ \b ->
+            checkTodo (s :: String) a (b :: Int) == Left TitleEmpty
 
-     modifyMaxSuccess (const 50000) $ prop "title is valid when not empty" $
-         \ t -> not (null t) ==> isTitleValid t == ValidTitle t
+    prop "must return invalid title ONLY IF title is empty or blank" $
+      \s a b -> (not . all isSpace) s ==> checkTodo s a (b :: Int) /= Left TitleEmpty
 
-     -- prop "title is not valid when it only contains blanks" $
-        -- \ t -> isTitleValid t == InvalidTitle
+    prop "never return PastDueDate when no input due date is provided" $
+      \s a -> checkTodo s Nothing (a :: Int) /= Left PastDueDate
+
+    prop "never return PastDueDate when no input due date is provided" $
+      \s a b -> (not . all isSpace) s && a < b ==> checkTodo s (Just a) (b :: Int) == Left PastDueDate
+
+    prop "always successful when title non empty/blank and due date not present (redundant test)" $
+      \s a -> (not . all isSpace) s ==> isRight $ checkTodo s Nothing (a :: Int)
+
+    prop "always successful when title non empty/blank and due date is present and in the future(redundant test)" $
+      \s a b -> (not . all isSpace) s && a > b ==> isRight $ checkTodo s (Just a) (b :: Int)
+
+  describe "trim" $ do
+    prop "must be idempotent" $
+      \s -> (trim . trim) s == trim s
+
+    prop "must always return empty string if the original string is empty or blank" $
+      forAll emptyOrBlankGen $ \s ->
+        trim s == ""
+
+    prop "must behave like the identify on the subset of strings not containing space characters" $
+      forAll notEmptyStringGen $ \s -> trim s == s
+
+    prop "must remove empty strings at the end and at the beginning but not in the middle" $
+      forAll (suchThat emptyOrBlankGen (not . null)) $ \s1 ->
+        forAll (suchThat notEmptyStringGen (not . null)) $ \s2 ->
+          forAll (suchThat emptyOrBlankGen (not . null)) $ \s3 ->
+            forAll (suchThat notEmptyStringGen (not . null)) $ \s4 ->
+              forAll (suchThat emptyOrBlankGen (not . null)) $ \s5 ->
+                trim (s1 ++ s2 ++ s3 ++ s4 ++ s5) == s2 ++ s3 ++ s4
+
+    prop "trim right should behave like trim left on the reversed string" $
+      \s -> trimRight s == (reverse . trimLeft . reverse) s
+
+    it "for example, must remove spaces at the beginning and at the end but not in the middle" $
+      trim "  he ll  o " `shouldBe` "he ll  o"
